3bdbc752dd4bcb78ce15e6c288faf53dfb923ad0
ampaire <femayapr@gmail.com>
2020-08-04 17:22:00 -0600

    user auth

---
+++ src/Components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is Default page</h3>
+      </div>
+    );
+  }
+}
